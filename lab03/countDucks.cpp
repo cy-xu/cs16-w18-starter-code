18a5ac85db269ee6a1f1a8e904c7e39681019300
Diba Mirza <dimirza@cs.ucsb.edu>
2018-02-04 19:26:30 -0800

    starter code for lab03

--- lab03/countDucks.cpp
+++ lab03/countDucks.cpp
@@ -10,5 +10,10 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+  if (argc!=2) {
+    // if argc is not 2, print an error message and exit
+    cerr << "Usage: "<< argv[0] << " inputFile" << endl;
+    exit(1); // defined in cstdlib
+  }
   return 0;
 }
