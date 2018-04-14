16c34b8159f78b6522cb58e98a889815321900b7
cy-xu <hello.xu@gmail.com>
2018-04-14 15:43:33 -0700

    Initialization

---
+++ lab01/block.cpp
@@ -0,0 +1,25 @@
+/*
+CS16 lab01 pratice
+CY Xu
+*/
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+void makeBlock(int row, int col) {
+  for (int i = 0; i < row; i++) {
+    for (int j = 0; j < col; j++) {
+      cout << "x.";
+    }
+    cout << endl;
+  }
+}
+
+int main() {
+  int row = 1, col = 1;
+  while (row != 0 && col != 0) {
+    cout << "Enter number o frows and columns:" << endl;
+    cin >> row >> col;
+    makeBlock(row, col);
+  }
+}
